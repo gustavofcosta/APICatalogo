a7c0450f69b422b20bc8e50ac2ce4fc4be93fbdf
gustavofcosta <gfcdesenvolvimento@gmail.com>
2024-10-23 23:38:05 -0300

    Add project files.

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+    }
+}
